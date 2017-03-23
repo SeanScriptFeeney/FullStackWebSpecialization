84da9eb0d1d86a541c43e02aa983e08c992b889b
Sean Feeney <seanfeeney@192.168.1.14>
2017-03-23 19:52:48 +0800

    scope added to controller

--- Angular JS/Module3/Gulp Exercise/conFusion/gulpfile.js
+++ Angular JS/Module3/Gulp Exercise/conFusion/gulpfile.js
@@ -12,7 +12,8 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
-    del = require('del');
+    del = require('del')
+    ngannotate = require('gulp-ng-annotate');
 
 
 gulp.task('jshint', function() {
@@ -33,11 +34,12 @@ gulp.task('default', ['clean'], function() {
 
 gulp.task('usemin',['jshint'], function () {
   return gulp.src('./app/menu.html')
-      .pipe(usemin({
-        css:[minifycss(),rev()],
-        js: [uglify(),rev()]
-      }))
-      .pipe(gulp.dest('dist/'));
+    .pipe(usemin({
+      css:[minifycss(),rev()],
+      js: [ngannotate(),uglify(),rev()]
+    }))
+    
+    .pipe(gulp.dest('dist/'));
 });
 
 // Images
